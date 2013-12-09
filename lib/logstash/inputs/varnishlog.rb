cutoffthetop/logstash

commit aa348392544962e9c6743c272a4ec514c9b6818e

- standardize of a few field names:   * host: the origin of the event (for file input, etc, many times this is     obvious. For network inputs, this is often the client ip address)   * path: the origin path of the event (file path, EventLog source, etc) - some misc style cleanup done while doing the above.

lib/logstash/inputs/varnishlog.rb
@@ -10,7 +10,6 @@ class LogStash::Inputs::Varnishlog < LogStash::Inputs::Threadable
   public
   def register
     require 'varnish'
-    @source = "varnishlog://#{Socket.gethostname}/"
     @vd = Varnish::VSM.VSM_New
     Varnish::VSL.VSL_Setup(@vd)
     Varnish::VSL.VSL_Open(@vd, 1)
@@ -19,6 +18,7 @@ class LogStash::Inputs::Varnishlog < LogStash::Inputs::Threadable
 
   def run(queue)
     @q = queue
+    @hostname = Socket.gethostname
     Varnish::VSL.VSL_Dispatch(@vd, self.method(:cb).to_proc, FFI::MemoryPointer.new(:pointer))
   end # def run
 
@@ -26,7 +26,7 @@ class LogStash::Inputs::Varnishlog < LogStash::Inputs::Threadable
   def cb(priv, tag, fd, len, spec, ptr, bitmap)
     begin
       str = ptr.read_string(len)
-      event = LogStash::Event.new("message" => str, "source" => @source)
+      event = LogStash::Event.new("message" => str, "host" => @host)
       event["varnish_tag"] = tag
       event["varnish_fd"] = fd
       event["varnish_spec"] = spec

lib/logstash/inputs/eventlog.rb
@@ -57,7 +57,8 @@ class LogStash::Inputs::EventLog < LogStash::Inputs::Base
         timestamp = to_timestamp(event.TimeGenerated)
 
         e = LogStash::Event.new(
-          "source" => "eventlog://#{@hostname}/#{@logfile}",
+          "host" => @hostname,
+          "path" => @logfile,
           "type" => @type,
           "@timestamp" => timestamp
         )

lib/logstash/inputs/exec.rb
@@ -34,6 +34,7 @@ class LogStash::Inputs::Exec < LogStash::Inputs::Base
 
   public
   def run(queue)
+    hostname = Socket.gethostname
     loop do
       start = Time.now
       @logger.info("Running exec", :command => @command) if @debug
@@ -41,7 +42,7 @@ class LogStash::Inputs::Exec < LogStash::Inputs::Base
       # out.read will block until the process finishes.
       @codec.decode(out.read) do |event|
         decorate(event)
-        event["source"] = "exec://#{Socket.gethostname}"
+        event["host"] = hostname
         event["command"] = @command
         queue << event
       end

lib/logstash/inputs/file.rb
@@ -127,10 +127,11 @@ class LogStash::Inputs::File < LogStash::Inputs::Base
     hostname = Socket.gethostname
 
     @tail.subscribe do |path, line|
-      source = "file://#{hostname}/#{path.gsub("\\","/")}"
       @logger.debug? && @logger.debug("Received line", :path => path, :line => line)
       @codec.decode(line) do |event|
         decorate(event)
+        event["host"] = hostname
+        event["path"] = path
         event["source"] = source
         queue << event
       end

lib/logstash/inputs/ganglia.rb
@@ -67,11 +67,10 @@ class LogStash::Inputs::Ganglia < LogStash::Inputs::Base
 
     loop do
       packet, client = @udp.recvfrom(9000)
-      # Ruby uri sucks, so don't use it.
-      source = "ganglia://#{client[3]}/"
-
+      # TODO(sissel): make this a codec...
       e = parse_packet(packet,source)
       unless e.nil?
+        e["host"] = client[3] # the IP address
         output_queue << e
       end
     end

lib/logstash/inputs/gelf.rb
@@ -83,7 +83,7 @@ class LogStash::Inputs::Gelf < LogStash::Inputs::Base
       end
 
       event = LogStash::Event.new(JSON.parse(data))
-      event["source"] = client[3]
+      event["host"] = client[3]
       if event["timestamp"].is_a?(Numeric)
         event["@timestamp"] = Time.at(event["timestamp"]).gmtime
         event.remove("timestamp")

lib/logstash/inputs/generator.rb
@@ -56,7 +56,6 @@ class LogStash::Inputs::Generator < LogStash::Inputs::Threadable
 
   def run(queue)
     number = 0
-    source = "generator://#{@host}/"
 
     if @message == "stdin"
       @logger.info("Generator plugin reading a line from stdin")
@@ -68,7 +67,7 @@ class LogStash::Inputs::Generator < LogStash::Inputs::Threadable
       @lines.each do |line|
         @codec.decode(line.clone) do |event|
           decorate(event)
-          event["source"] = source
+          event["host"] = @host
           event["sequence"] = number
           queue << event
         end
@@ -78,7 +77,7 @@ class LogStash::Inputs::Generator < LogStash::Inputs::Threadable
 
     if @codec.respond_to?(:flush)
       @codec.flush do |event|
-        event["source"] = source
+        event["host"] = @hos
         queue << event
       end
     end

lib/logstash/inputs/irc.rb
@@ -38,11 +38,6 @@ class LogStash::Inputs::Irc < LogStash::Inputs::Base
   # "#logstash".
   config :channels, :validate => :array, :required => true
 
-
-  def initialize(*args)
-    super(*args)
-  end # def initialize
-
   public
   def register
     require "cinch"

commit 33bed59f79f8815f26d73850f811a542d951f1e8

Fix varnishlog input host field
Commit aa348392544962e9c6743c272a4ec514c9b6818e introduced new
variable names and broke the host field.

lib/logstash/inputs/varnishlog.rb
@@ -19,7 +19,7 @@ class LogStash::Inputs::Varnishlog < LogStash::Inputs::Threadable
 
   def run(queue)
     @q = queue
-    @hostname = Socket.gethostname
+    @host = Socket.gethostname
     Varnish::VSL.VSL_Dispatch(@vd, self.method(:cb).to_proc, FFI::MemoryPointer.new(:pointer))
   end # def run
 